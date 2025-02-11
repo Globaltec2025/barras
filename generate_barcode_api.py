4d531a687b5412157bf6c7e3fc51aa9f030e6ead
Globaltec2025 <asistente.sistemas@globaltec.com.pe>
2025-02-11 16:31:20 -0500

    Update generate_barcode_api.py

--- generate_barcode_api.py
+++ generate_barcode_api.py
@@ -3,9 +3,11 @@ import requests
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import mm
-from reportlab.lib.utils import simpleSplit
+from pdf2image import convert_from_bytes
 import io
 import os
+import fitz  # PyMuPDF
+from PIL import Image
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -84,69 +86,6 @@ def buscar():
         return jsonify(datos)
     else:
         return jsonify({'error': 'No se encontraron datos para el código proporcionado.'})
-
-@app.route('/generar_pdf', methods=['POST'])
-def generar_pdf():
-    data = request.json
-    codigo = data.get('code')
-    cantidad = int(data.get('quantity'))
-    descripcion = data.get('description')
-    marca = data.get('marca')
-    medida = data.get('medida')
-
-    # Crear el buffer para el archivo PDF
-    buffer = io.BytesIO()
-    custom_size = (96 * mm, 27 * mm)  # Ancho x Alto en milímetros
-    p = canvas.Canvas(buffer, pagesize=custom_size)
-
-    # Rutas de las imágenes
-    logo_path = os.path.join(app.static_folder, 'imagenes/logo.jpg')
-    new_code_path = os.path.join(app.static_folder, 'imagenes/new_code1.png')
-
-    # Estilos de texto
-    font_name = "Helvetica-Bold"
-    code_font_size = 12  # Tamaño de letra para el código
-    description_font_size = 7  # Tamaño de letra para la descripción
-    marca_font_size = 7  # Tamaño de letra para la marca
-    medida_font_size = 7  # Tamaño de letra para la medida
-    max_width = 90 * mm  # Ancho máximo para dividir texto
-    line_spacing = 2.7 * mm  # Espaciado entre líneas
-
-    # Generar el PDF para la cantidad solicitada
-    for _ in range(cantidad):
-        # Dibujar imágenes
-        p.drawImage(logo_path, 2, 18.5, width=9 * mm, height=11 * mm)
-        p.drawImage(new_code_path, 30, 18.5, width=95 * mm, height=15 * mm)
-
-        # Dibujar código
-        p.setFont(font_name, code_font_size)  # Tamaño de letra ajustado para código
-        p.drawString(37 * mm, 3 * mm, f"{codigo}")
-
-        # Dibujar descripción
-        p.setFont(font_name, description_font_size)  # Tamaño de letra ajustado para descripción
-        lines = simpleSplit(descripcion, font_name, description_font_size, max_width)
-
-        y_position = 24.5 * mm
-        for line in lines:
-            p.drawString(4 * mm, y_position, line)
-            y_position -= line_spacing
-
-        # Dibujar marca
-        p.setFont(font_name, marca_font_size)  # Tamaño de letra para marca
-        p.drawString(80 * mm, 0.2 * mm, f"{marca}")
-
-        # Dibujar medida
-        p.setFont(font_name, medida_font_size)  # Tamaño de letra para medida
-        p.drawString(2 * mm, 0.2 * mm, f"{medida}")
-
-        # Crear nueva página
-        p.showPage()
-
-    # Guardar y devolver el archivo PDF
-    p.save()
-    buffer.seek(0)
-    return send_file(buffer, as_attachment=True, download_name='codigo_sap.pdf', mimetype='application/pdf')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
